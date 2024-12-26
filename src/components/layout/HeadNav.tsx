b8b918de184e2791abc3498d699af351e908c657
지영 <reizvoll@jiyeong-ui-MacBookAir.local>
2024-12-26 10:24:51 +0900

    feat: 레이아웃 컴포넌트, CSS 통일화 및 누락된 기능 수정

--- src/components/layout/HeadNav.tsx
+++ src/components/layout/HeadNav.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import { GoHome } from 'react-icons/go'
-import { MdOutlineBookmarks } from 'react-icons/md'
-import ClientSideActions from './SideActionClient'
+import  ProtectedLogin from './ProtectedLogin'
 import ModalHandler from './ModalHandler'
 import { User } from '@/types/user'
+import ProtectedBookmarks from './ProtectedBookmarks'
 
 interface HeadNavProps {
   user: User | null
@@ -11,16 +11,14 @@ interface HeadNavProps {
 
 export default function HeadNav({ user }: HeadNavProps) {
   return (
-    <div className="absolute top-5 right-5 w-[242px] h-[50px] bg-[rgba(137,137,137,0.4)] backdrop-blur-[2px] rounded-[30px] flex items-center justify-around px-5 z-30">
+    <div className="absolute top-5 right-5 w-[242px] h-[50px] bg-[#898989] bg-opacity-40 backdrop-blur-[2px] rounded-[30px] flex items-center justify-around px-5 z-30">
       <Link href="/" passHref>
         <GoHome className="text-white w-[30px] h-[30px] cursor-pointer" />
       </Link>
 
-      <Link href="/mypage" passHref>
-        <MdOutlineBookmarks className="text-white w-[25px] h-[25px] cursor-pointer" />
-      </Link>
+      <ProtectedBookmarks user={user} />
 
-      <ClientSideActions user={user} />
+      <ProtectedLogin user={user} />
       <ModalHandler />
     </div>
   )
