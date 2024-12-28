a19c3c5d96559cc4c47bec590519a41e6bc901b4
leekyoungmin <zzxx66052@gmail.com>
2024-12-29 01:44:27 +0900

    feat: user Info Loading..

--- src/app/(dashboard)/comment/[id]/page.tsx
+++ src/app/(dashboard)/comment/[id]/page.tsx
@@ -1,30 +1,42 @@
 'use client'
 
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 
 import CardInfo from '@/components/comment/CardInfo'
-import CommentForm from '@/components/comment/CommentForm'
 import { useAuthStore } from '@/store/auth'
+import { useEffect } from 'react'
 
 export default function CommentPage() {
   const params = useParams()
+  const router = useRouter()
   const postId = params?.id as string
 
   const user = useAuthStore((state) => state.user)
-  const userId = user!.id
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/auth/login')
+    }
+  }, [user, router])
+
+  if (!user) {
+    return null
+  }
+  const userId = user.id
 
   if (!postId) {
-    return <div>잘못된 접근입니다. 게시글 ID가 없습니다.</div>
-  }
-
-  const handleCommentSubmit = (comment: string) => {
-    console.log('댓글 작성:', comment)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold text-center">
+          잘못된 접근입니다. 게시글 ID가 없습니다.
+        </h1>
+      </div>
+    )
   }
 
   return (
     <div className="px-4 py-8 max-w-[1200px] mx-auto">
       <CardInfo postId={postId} userId={userId} />
-      <CommentForm onCommentSubmit={handleCommentSubmit} />
     </div>
   )
 }
