00ff8d18e743be8ff80c2356ba9524725bf07785
leekyoungmin <zzxx66052@gmail.com>
2024-12-29 18:17:33 +0900

    feat: user 정보 불러오기

--- src/components/mypage/CreateCardList.tsx
+++ src/components/mypage/CreateCardList.tsx
@@ -1,2 +1,12 @@
-// 여기 부탁함 케이리.. 음 디자인은.. ye... 피그마보고 하셔.. 
+'use client'
+
+import { useAuthStore } from '@/store/auth'
+import { useRouter } from 'next/navigation'
+
+// 여기 부탁함 케이리.. 음 디자인은.. ye... 피그마보고 하셔..
 // (정작 대장은 따끈-한 카드 냅다 가져와벌임 ^-^...)
+
+export default function MyPageCards() {
+  const user = useAuthStore((state) => state.user)
+  const router = useRouter()
+}
