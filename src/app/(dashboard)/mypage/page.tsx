f70be9deaf9aad30d4986241a696f14eb8e91787
림졍 <mara2277@naver.com>
2024-12-29 17:33:51 +0900

    feat: Mypage.tsx, Bookmark된 게시글 기능 구한 완.(아직 산넘어 산인 작성부분..)

--- src/app/(dashboard)/mypage/page.tsx
+++ src/app/(dashboard)/mypage/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import BookmarkedCardList from '@/components/mypage/BookmarkedCardList';
 import MyPageProfile from '@/components/mypage/MyPageProfile';
 
 export default function MyPage() {
@@ -7,6 +8,7 @@ export default function MyPage() {
       <div className="max-w-custom mx-auto p-8">
         {/* 프로필 섹션 */}
         <MyPageProfile />
+        <BookmarkedCardList />
       </div>
   );
 }
